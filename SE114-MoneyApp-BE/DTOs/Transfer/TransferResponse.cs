57419c5662bd47d3870e70f86f84c904c4455624
nguyenhun11 <giahungcantho11@gmail.com>
2026-06-21 01:40:44 +0700

    feat: add color and icon to transfer response

--- SE114-MoneyApp-BE/DTOs/Transfer/TransferResponse.cs
+++ SE114-MoneyApp-BE/DTOs/Transfer/TransferResponse.cs
@@ -7,8 +7,12 @@ namespace SE114_MoneyApp_BE.DTOs.Transfer
         public Guid Id { get; set; }
         public Guid SourceAccount { get; set; }
         public string SourceAccountName { get; set; } = string.Empty;
+        public int SourceAccountColorId { get; set; }
+        public int SourceAccountIconId { get; set; }
         public Guid DestinationAccount { get; set; }
         public string DestinationAccountName { get; set; } = string.Empty;
+        public int DestinationAccountColorId { get; set; }
+        public int DestinationAccountIconId { get; set; }
         public decimal Amount { get; set; }
         public DateTime TransferDate { get; set; }
         public string Description { get; set; } = string.Empty;
